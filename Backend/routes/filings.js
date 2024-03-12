f27c4f02324b872655d9aa4f00519d6b2bc5495d
Udit Yadav <udityadav133@gmail.com>
2024-03-12 22:18:30 +0530

    tested all api methods in backend

--- Backend/routes/filings.js
+++ Backend/routes/filings.js
@@ -6,12 +6,24 @@ router.get('/', (req, res) => {
     res.json({ msg: "GET All Filings" })
 })
 
+//NOTE - GET a Single Filing
+router.get('/:id', (req, res) => {
+    res.json({ msg: "GET a Single  Filings" })
+})
 
 //NOTE - POST
-
+router.post('/', (req, res) => {
+    res.json({ msg: "POST All Filings" })
+})
 //NOTE - DELETE
+router.delete('/:id', (req, res) => {
+    res.json({ msg: "DELETE All Filings" })
+})
 
 //NOTE - PATCH
+router.patch('/:id', (req, res) => {
+    res.json({ msg: "PATCH All Filings" })
+})
 
 
 module.exports = router;
